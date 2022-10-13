c89491008f1767d35c8aa54c200946705c507ab2
CraigYeoman <yeomancraig@gmail.com>
2022-10-12 19:43:52 -0500

    Background for hover and css changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,16 @@
+import sauron from "../images/sauron2.gif";
+
 const Card = ({cards}) => {
-    return cards.map((card) => (
+  function changeBackground(e) {
+    e.target.style.background = 'red'
+  }  
+  
+  return cards.map((card) => (
         <div 
           key={card.id}
           id={card.id}
           className="card--container"
+          
         >
           <img src={card.picture} className="card--pic" id={card.id}/>
           <div className="card--name">{card.name}</div>
